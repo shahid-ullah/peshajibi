b6f8d2216b8c5ba80f22c7f81c947eeadccb3c7e
shahid <shahid.ice.nstu@gmail.com>
2023-01-30 12:09:25 +0600

    change admin panel text

--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework.authtoken.views import obtain_auth_token
 
 from users.apis import ProfileUpdateAPI, RegistrationAPI, UserDetailAPI, UserListAPI
+
+# from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -39,3 +41,8 @@ if settings.DEBUG:
     urlpatterns += optional_urlpatterns
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+admin.site.site_header = "Peshajibi admin"
+admin.site.site_title = "Peshajibi admin"
+admin.site.index_title = "Welcome to Peshajibi Admin Panel"
