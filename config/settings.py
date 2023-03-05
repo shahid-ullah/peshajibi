7417de4abe849510075c0a85ecc3752809a5ab98
shahid <shahid.ice.nstu@gmail.com>
2023-03-05 11:38:07 +0600

    update allowed_host

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,8 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://peshajibi.tappware.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'peshajibi.tappware.com']
+CSRF_TRUSTED_ORIGINS = ['https://peshajibi.tappware.com', 'localhost', '127.0.01']
 
 
 # Application definition
