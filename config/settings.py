b0b5be4699f5bf9384dfde11475f8541819706bb
shahid <shahid.ice.nstu@gmail.com>
2023-03-05 11:27:50 +0600

    set debug false

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-+5k)u=4f9_^-3l^q7g8^!gv9u*5i68vyu@t3y(fw7r*$5#5=ei
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'peshajibi.tappware.com']
 
